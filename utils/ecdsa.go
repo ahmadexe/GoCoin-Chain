ccc58f705cd35d91ded06f4ab1b3884cb530fada
ahmadexe <muahmad710@gmail.com>
2023-07-26 00:17:51 +0500

    string to big.Int generating x and y for ecdsa

--- utils/ecdsa.go
+++ utils/ecdsa.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/hex"
 	"fmt"
 	"math/big"
 )
@@ -12,4 +13,17 @@ type Signature struct {
 
 func (s *Signature) String() string {
 	return fmt.Sprintf("%x%x", s.R, s.S)
+}
+
+func StringToBigIntTuple(str string) (big.Int, big.Int) {
+	bx, _ := hex.DecodeString(str[:64])
+	by, _ := hex.DecodeString(str[64:])
+
+	x := big.Int{}
+	_ = x.SetBytes(bx)
+
+	y := big.Int{}
+	_ = y.SetBytes(by)
+
+	return x, y
 }
